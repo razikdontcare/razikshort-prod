255aeeec42b17af66732d596e0e1971383a1cb1c
Razik <GamerSia4@gmail.com>
2022-08-09 00:44:12 +0800

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,12 +1,7 @@
 import {initializeApp} from 'firebase/app'
 
 const firebaseConfig = {
-    "apiKey": "AIzaSyDriS1994TEyVwHV4ViDcR1BGvBnncGrII",
-    "authDomain": "fnestch.firebaseapp.com",
-    "databaseURL": "https://fnestch.firebaseio.com",
-    "messagingSenderId": "7678039539",
-    "projectId": "fnestch",
-    "storageBucket": "fnestch.appspot.com"
+    // your firebase config
 }
 
 const app = initializeApp(firebaseConfig)
