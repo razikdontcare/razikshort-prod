48198b9de77719a4066a4ce04c299ebd3b88c0a4
Razik <GamerSia4@gmail.com>
2022-08-21 00:38:39 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import {ChakraProvider} from '@chakra-ui/react'
-import Navbar from '../components/navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (<>
     <ChakraProvider>
-      {/* <Navbar /> */}
       <Component {...pageProps} />
     </ChakraProvider>
   </>)
